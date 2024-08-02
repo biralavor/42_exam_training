a2466a98c6ac50ca47d164ecf008067f8b52f0e1
biralavor <umeneses@student.42sp.org.br>
2024-08-02 16:20:26 -0300

    optimizing code logic

--- Exam_Rank_02/Level_0/rot_13/rot_13.c
+++ Exam_Rank_02/Level_0/rot_13/rot_13.c
@@ -6,89 +6,30 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:10:12 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/02 15:10:14 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/02 16:18:13 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include <unistd.h>
 
-void	ft_putchar(char c);
-void	rot_13(const char *string);
-bool	ft_isalpha(int c);
-bool	ft_isupper(int c);
-bool	ft_islower(int c);
-
-int	main(int argc, char **argv)
-{
-	int	index;
-
-	index = 1;
-	if (argc == 2)
-		rot_13(argv[index]);
-	ft_putchar('\n');
-	return (0);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	rot_13(const char *str)
+int	main(int ac, char **av)
 {
 	int	idx;
-	int	cript = 13;
-	int	up_limit = 90;
-	int	low_limit = 122;
-	int	up_range = up_limit - cript + 1;
-	int	low_range = low_limit - cript + 1;
-	int	temp;
 
-	idx = -1;
-	temp = 0;
-	while(str[++idx] != '\0')
-	{
-		if (ft_isalpha(str[idx]))
+	idx = 0;
+	if (ac == 2)
 		{
-
-			if (ft_isupper(str[idx]))
+			while (av[1][idx] != '\0')
 			{
-				if (str[idx] >= up_range)
-				{
-					temp = str[idx] - up_range;
-					ft_putchar(temp + 65);
-				}
-				else
-					ft_putchar(str[idx] + cript);
-			}
-			else if (ft_islower(str[idx]))
-			{
-				if (str[idx] >= low_range)
-				{
-					temp = str[idx] - low_range;
-					ft_putchar(temp + 97);
-				}
-				else
-					ft_putchar(str[idx] + cript);
+				if (av[1][idx] >= 'a' && av[1][idx] <= 'z')
+					av[1][idx] = (av[1][idx] + 13 - 'a') % 26 + 'a';
+				if (av[1][idx] >= 'A' && av[1][idx] <= 'Z')
+					av[1][idx] = (av[1][idx] + 13 - 'A') % 26 + 'A';
+				write(1, &av[1][idx], 1);
+				idx++;
 			}
 		}
-		else
-			ft_putchar(str[idx]);
-	}
-}
-
-bool	ft_isalpha(int c)
-{
-	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
-}
-
-bool	ft_isupper(int c)
-{
-	return (c >= 65 && c <= 90);
-}
-
-bool	ft_islower(int c)
-{
-	return (c >= 97 && c <= 122);
+	write(1, "\n", 1);
+	return (0);
 }
