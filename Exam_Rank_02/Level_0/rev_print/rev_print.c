66a04d85eb814385a4545b389f1b0eda506786a7
biralavor <umeneses@student.42sp.org.br>
2024-08-03 08:21:10 -0300

    improving rev_print

--- Exam_Rank_02/Level_0/rev_print/rev_print.c
+++ Exam_Rank_02/Level_0/rev_print/rev_print.c
@@ -6,33 +6,29 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 15:05:10 by umeneses          #+#    #+#             */
-/*   Updated: 2024/08/02 15:05:21 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/08/03 08:18:56 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strlen(char *s)
+char	*rev_print(char *s)
 {
-	int	idx = 0;
-	while(s[idx] != '\0')
-		idx++;
-	return(idx);
-}
-
-void	rev_print(char *av)
-{
-	int	len = ft_strlen(av) - 1;
-	while(len >= 0)
+	int	len = 0;
 	{
-		write(1, &av[len], 1);
-		len--;
+		while(s[len] != '\0')
+			len++;
+		while(--len >= 0)
+			write(1, &s[len], 1);
 	}
+	return (s);
 }
 
 int	main (int ac, char **av)
 {
+	
 	if (ac == 2)
 		rev_print(av[1]);
 	write(1, "\n" , 1);
+	return (0);
 }
